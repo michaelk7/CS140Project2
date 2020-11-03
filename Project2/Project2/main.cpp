d351de14ae45215799e88dcc6d606519494ad1aa
Michael Kelnar <michaelkelnar7@gmail.com>
2020-11-03 11:25:44 -0600

    Created the project in xcode and added the starting code

---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Michael Kelnar on 11/3/20.
+//  Copyright © 2020 Michael Kelnar. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+}
